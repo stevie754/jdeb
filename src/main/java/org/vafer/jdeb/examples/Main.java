371ee5b8dffd32485cf5babb11ce7a1eaa61cd1e
Stephen Mitchell <stevie754@gmail.com>
2016-06-06 14:51:47 +0100

    3

--- src/main/java/org/vafer/jdeb/examples/Main.java
+++ src/main/java/org/vafer/jdeb/examples/Main.java
@@ -3,5 +3,6 @@ package org.vafer.jdeb.examples;
 public class Main {
     public static void main(String[] args) {
         System.out.println("jdeb example!");
+        //Making a test change
     }
 }
